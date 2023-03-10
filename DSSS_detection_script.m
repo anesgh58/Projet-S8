7bc8299bdbb01e7b9d38862991161bec25241831
anesgh58 <75451146+anesgh58@users.noreply.github.com>
2023-03-10 17:57:38 +0100

    Add files via upload
    Génération de l'enveloppe complexe d'un signal DSSS ainsi que détection de cette modulation.

---
+++ DSSS_detection_script.m
@@ -0,0 +1,43 @@
+%% Projet S8 
+clear;
+clc;
+close all
+
+%% Parametres
+N = 2^10;                                                     % Nombre de bits 
+B = 22e3;                                                     % Largeur de bande du signal
+fe = 50e3;                                                    % Fréquence d'échantillonage
+roll_off = 0.5;                                               % Facteur de roll-off
+
+%% Tx
+% DSSS
+[yl,~,~] = DSSS(N,B,fe,roll_off);                             % Signal DSSS
+
+% Modulation QPSK
+sequence = randn(1,length(yl)) > 0;                           % Géneration d'une séquence binaire aléatoire
+psk = pskmod(bi2de(sequence.'),4,pi/4).';                     % Modulation QPSK de la séquence binaire aléatoire
+
+% Signal recu
+signal_recu_DSSS = yl ;                                       % DSSS 
+signal_recu_mixte = yl + psk;                                 % DSSS + QPSK   
+
+%% DSSS detection
+[pho_DSSS,Lc_est_DSSS] = DSSS_detection(signal_recu_DSSS);    % Détection d'un signal DSSS emis seul
+[pho_mixte,Lc_est_mixte] = DSSS_detection(signal_recu_mixte); % Détection d'un signal DSSS émis avec d'autres signaux
+assert(Lc_est_DSSS == Lc_est_mixte);
+%% Figures
+% Moyenne des moments d’ordre 2 de la fonction d’autocorrélation des différents sous signaux
+
+figure,
+plot(pho_DSSS)
+xlabel('Échantillons')
+ylabel('Amplitude')
+title("Détection d'un signal DSSS emis seul")
+
+figure,
+plot(pho_mixte)
+xlabel('Échantillons')
+ylabel('Amplitude')
+title("Détection d'un signal DSSS émis avec d'autres signaux")
+
+
