c29c47e66c023e7f1f92b68828ceddfc1836a614
unknown <nhlaitabbou@gmail.com>
2023-03-24 15:12:05 +0100

    Interface graphique

--- onde_AM.m
+++ onde_AM.m
@@ -26,47 +26,47 @@ if strcmp(type,'BLU')
     s= m.*cos(2*pi*Fp*t) + m.*sin(2*pi*Fp*t);
 end
 
-%% Calcul du périodogramme
-
-[Perio,f] = periodogram(s,'centered');
-
-figure,
-plot(f,Perio);
-xlabel('Fréquence (Hz)');
-ylabel('Densité spectrale de puissance');
-grid on;
-title('Periodogramme du signal');
-
-%% Calcul de la transformée de Fourrier
-
-TF=fft(s);
-
-figure,
-plot(real(TF));
-grid on;
-title("Transformée de Fourier du signal");
-
-
-%% Affichage
-
-figure,
-subplot(3,1,1);
-plot(t,p);
-xlabel('Temps');
-xlim([0 3*Tsm]);
-title('Représentation de la porteuse');
-grid on;
-
-subplot(3,1,2);
-plot(t,m);
-xlabel('Temps');
-xlim([0 3*Tsm]);
-title('Représentation du modulant');
-grid on;
-
-subplot(3,1,3);
-plot(t,s);
-xlim([0 3*Tsm]);
-xlabel('Temps');
-title('Représentation du signal modulé en amplitude')
-grid on;
+% %% Calcul du périodogramme
+% 
+% [Perio,f] = periodogram(s,'centered');
+% 
+% figure,
+% plot(f,Perio);
+% xlabel('Fréquence (Hz)');
+% ylabel('Densité spectrale de puissance');
+% grid on;
+% title('Periodogramme du signal');
+% 
+% %% Calcul de la transformée de Fourrier
+% 
+% TF=fft(s);
+% 
+% figure,
+% plot(real(TF));
+% grid on;
+% title("Transformée de Fourier du signal");
+% 
+% 
+% %% Affichage
+% 
+% figure,
+% subplot(3,1,1);
+% plot(t,p);
+% xlabel('Temps');
+% xlim([0 3*Tsm]);
+% title('Représentation de la porteuse');
+% grid on;
+% 
+% subplot(3,1,2);
+% plot(t,m);
+% xlabel('Temps');
+% xlim([0 3*Tsm]);
+% title('Représentation du modulant');
+% grid on;
+% 
+% subplot(3,1,3);
+% plot(t,s);
+% xlim([0 3*Tsm]);
+% xlabel('Temps');
+% title('Représentation du signal modulé en amplitude')
+% grid on;
